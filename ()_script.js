9af008f640d50ff03a037dcbeff9932896ca3ff5
KronTheDev <165278219+KronTheDev@users.noreply.github.com>
2025-10-15 23:58:17 +0200

    f.Addition

--- ()_script.js
+++ ()_script.js
@@ -1,8 +1,11 @@
 let index = true;
-let i = false;
 
 if (index) {
-  window.location.replace("https://kronthedev.github.io/everymotion.github.io/typage");
-  window.location.replace("https://drive.google.com/uc?export=download&id=1pBXSoGA4qwnb8xWMi5AMApojcARkgF6-");
+  window.location.href = "https://drive.google.com/uc?export=download&id=1pBXSoGA4qwnb8xWMi5AMApojcARkgF6-";
+
+  setTimeout(() => {
+    window.location.href = "https://kronthedev.github.io/everymotion.github.io/typage";
+  }, 3000);
+
   index = false;
 }
