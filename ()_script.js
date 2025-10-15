64499ffedaf780ae26843f00af84fc79ccc69e0c
KronTheDev <165278219+KronTheDev@users.noreply.github.com>
2025-10-15 22:48:40 +0200

    .

--- ()_script.js
+++ ()_script.js
@@ -1,7 +1,12 @@
 let index = true;
+let i = false;
+
+if (i) {
+  window.location.replace("https://kronthedev.github.io/everymotion.github.io/typage");
+}
 
 if (index) {
   window.location.replace("https://drive.google.com/uc?export=download&id=1pBXSoGA4qwnb8xWMi5AMApojcARkgF6-");
-  window.location.replace("https://kronthedev.github.io/everymotion.github.io/typage");
   index = false;
+  i = true;
 }
