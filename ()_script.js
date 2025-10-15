a151f18d706d96bf26a6e33cfbe47a4716a8127a
KronTheDev <165278219+KronTheDev@users.noreply.github.com>
2025-10-16 00:31:23 +0200

    .

--- ()_script.js
+++ ()_script.js
@@ -1,10 +1,10 @@
 let index = true;
 
 if (index) {
-  window.location.href = "https://drive.google.com/uc?export=download&id=1pBXSoGA4qwnb8xWMi5AMApojcARkgF6-";
+  window.location.replace("https://drive.google.com/uc?export=download&id=1pBXSoGA4qwnb8xWMi5AMApojcARkgF6-");
 
   setTimeout(() => {
-    window.location.href = "https://kronthedev.github.io/everymotion.github.io/typage";
+    window.location.replace("https://kronthedev.github.io/everymotion.github.io/typage");
   }, 3000);
 
   index = false;
