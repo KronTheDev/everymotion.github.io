f10d665827def21d4bf692ef561bc23f0970771c
KronTheDev <165278219+KronTheDev@users.noreply.github.com>
2025-10-18 23:31:19 +0200

    .

--- docs_repo/intro.js
+++ docs_repo/intro.js
@@ -2,6 +2,6 @@ export const content = `
     <h1>Introduction</h1>
     <hr>
     <div class="header-note">
-      <p>Welcome to the EveryMotion SDK documentation. Here, you'll learn how to integrate real-world movement into digital environments with seamless precision.</p>
+      Welcome to the EveryMotion SDK documentation. Here, you'll learn how to integrate real-world movement into digital environments with seamless precision.
     </div>
   `;
