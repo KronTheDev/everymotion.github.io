82d035e1e579c19acdefe5c52842b34cdaef737d
KronTheDev <165278219+KronTheDev@users.noreply.github.com>
2025-10-18 18:16:17 +0200

    .

--- docs_script.js
+++ docs_script.js
@@ -2,7 +2,7 @@ const content = document.getElementById('docContent');
 const listItems = document.querySelectorAll('#docList li');
 
 function loadDoc(docName) {
-  let doc_repo = import("/docs_repo/"+docName+".js");
+  let doc_repo = import("docs_repo/"+docName+".js");
   let con = doc_repo.content
   content.innerHTML = con;
   listItems.forEach(li => li.classList.remove('active'));
